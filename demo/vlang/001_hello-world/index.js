765a202bb2ad88f71820f1246d03b4bee42c114a
footearth <footearth@gmail.com>
2023-08-19 03:21:42 +0800

    completed vlang

---
+++ demo/vlang/001_hello-world/index.js
@@ -0,0 +1,34 @@
+const wasmBytes = await Deno.readFile('./hello.wasm')
+
+const get_string = (ptr, len) => {
+  const buf = new Uint8Array(memory.buffer, ptr, len)
+  const str = new TextDecoder("utf8").decode(buf)
+
+  return str
+}
+
+const result = await WebAssembly.instantiate(
+    wasmBytes
+  , {
+      env: {
+        __writeln: (ptr, len) => {
+          console.log(get_string(ptr, len))
+        }
+      , __panic_abort: (ptr, len) => {
+          throw get_string(ptr, len);
+        }
+      }
+    }
+  )
+
+const memory = result.instance.exports['memory']
+
+// console.log(memory)
+
+console.time('main.main')
+
+const main = result.instance.exports['main.main']
+
+main()
+
+console.timeEnd('main.main')
