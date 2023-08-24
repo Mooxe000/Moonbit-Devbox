5c2a809434eda02baed31cf77471e553e1799aae
footearth <footearth@gmail.com>
2023-08-24 10:53:32 +0800

    turn to vlang

--- FrontEnd/DPVW/demo/src/index.js
+++ FrontEnd/DPVW/demo/src/index.js
@@ -1,25 +1,20 @@
-// import Hello from './pages/index.js'
-
-// van.add(document.body, Hello())
-//- van.add(document.getElementById('root'), Hello())
-
-import {Asdom} from 'https://esm.sh/asdom@0.2.2/glue'
-import {instantiate} from 'https://esm.sh/@assemblyscript/loader@0.27.9'
-
-const main = async () => {
-
-	const asdom = new Asdom()
-
-	const {exports} = await instantiate(
-    fetch('/index.wasm')
-  , {
-		...asdom.wasmImports
-	})
-
-	asdom.wasmExports = exports
-
-	exports._start()
-
+const env = {
+	console_log: s => console.log(s)
+, __writeln: (ptr, len) => {
+		console.log(get_string(ptr, len))
+	}
+, __panic_abort: (ptr, len) => {
+		throw get_string(ptr, len)
+	}
 }
 
-main()
+const res = await WebAssembly.instantiateStreaming(
+	fetch("index.wasm")
+, { env }
+)
+
+memory = res.instance.exports['memory']
+
+console.time('main.main')
+res.instance.exports['main.main']()
+console.timeEnd('main.main')
---
+++ FrontEnd/DPVW/demo/wasm/index.v
@@ -0,0 +1,5 @@
+fn JS.console_log(s &string)
+
+pub fn main() {
+	JS.console_log('Hello WASI!')
+}
