ab463bc4159bd6230289a5f2c91741416f4ebf1c
footearth <footearth@gmail.com>
2023-08-26 12:14:13 +0800

    adjust grain

--- demo/success/grain/001_hello-world/deno.js
+++ demo/success/grain/001_hello-world/deno.js
@@ -1,5 +1,5 @@
 // import Context from "./snapshot_preview1.ts";
-import Context from "https://deno.land/std@0.198.0/wasi/snapshot_preview1.ts";
+import Context from "https://deno.land/std@0.200.0/wasi/snapshot_preview1.ts";
 
 const options = JSON.parse(Deno.args[0])
 const pathname = Deno.args[1]
@@ -7,13 +7,26 @@ const binary = await Deno.readFile(pathname)
 const module = await WebAssembly.compile(binary)
 
 const context = new Context({
-  env: options.env
+  env: {
+    ...options.env
+  }
 , args: [pathname].concat(options.args)
 , preopens: options.preopens
 })
 
 const instance = new WebAssembly.Instance(module, {
   "wasi_snapshot_preview1": context.exports
+, jsapi: {
+    console_log: s => {
+        s === -2
+      ? console.log(true)
+      : s === 2147483646
+      ? console.log(false)
+      : s % 2 === 0
+      ? console.log(s)
+      : console.log((s - 1) / 2)
+    }
+  }
 })
 
 context.start(instance)
