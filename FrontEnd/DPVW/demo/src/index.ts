2ed15e916d699da8bf962242aa2a023fc9164bfa
footearth <footearth@gmail.com>
2023-08-29 15:33:01 +0800

    adjust for wasm

--- FrontEnd/DPVW/demo/src/index.ts
+++ FrontEnd/DPVW/demo/src/index.ts
@@ -1,15 +1,17 @@
-import { ZigJS } from './lib/zig-js/zigjs.ts';
+// import { ZigJS } from './lib/zig-js/zigjs.ts';
+// import { ZigJS } from 'https://esm.sh/gh/mitchellh/zig-js@60ac42ab13/js/src/index.ts';
+import { ZigJS } from 'https://esm.sh/zig-js-glue@0.1.3'
 
 export function add(a: number, b: number): number {
-  return a + b;
+  return a + b
 }
 
-const zjs = new ZigJS();
+const zjs = new ZigJS()
 const importObject = {
-  module: {},
-  env: {},
-  ...zjs.importObject(),
-};
+  module: {}
+, env: {}
+, ...zjs.importObject()
+}
 
 // const url = new URL('index.wasm', import.meta.url);
 // fetch(url.href).then(response =>
@@ -17,10 +19,10 @@ const res = await fetch('/static/index.wasm')
 const bytes = await res.arrayBuffer()
 const results = await WebAssembly.instantiate(bytes, importObject)
 
-const { memory, alert, set_title } = results.instance.exports;
+const { memory, alert, set_title } = results.instance.exports
 
-zjs.memory = memory;
+zjs.memory = memory
 
 // Call whatever example you want:
-set_title();
-alert();
+set_title()
+alert()
---
+++ FrontEnd/DPVW/demo/wasm.new/build.js
@@ -0,0 +1,1 @@
+'https://github.com/mitchellh/zig-js/archive/refs/heads/main.zip'
